170f68ba6978f30db22b6be4a70a77ec985b35ac
Arka-h <47897466+Arka-h@users.noreply.github.com>
2019-04-03 00:39:00 +0530

    Delete JavaScript1.js

--- JavaScript1.js
+++
@@ -1,7 +0,0 @@
-function form1()
-{
-	var login_id = document.getElementById("login_id").value;
-	var password = document.getElementById("password").value;
-
-	document.write("<h1> April Fools day!!!</h1>");
-}
